29c2765d63bcc4629803445cc162945550865276
logust79 <logust@yahoo.com>
2016-12-21 09:10:21 +0000

    HPO:add genes for construction. Genes: change gene_id to entrez_id, and add some special cases where mygene fails to correctly map. For these, add _bad_genes to avoid repetitively querying.

--- Genes.py
+++ Genes.py
@@ -9,18 +9,38 @@ import re
 def _initiate_db(db_conn):
     db_c = db_conn.cursor()
     db_c.execute('''CREATE TABLE IF NOT EXISTS genes
-        (id text NOT NULL UNIQUE, gene_id text, pLI real, mis_z real, genomic_pos_hg19 text, genomic_pos text, symbol text, alias text, PRIMARY KEY (id, gene_id))''')
+        (id text NOT NULL UNIQUE, entrez_id text, pLI real, mis_z real, genomic_pos_hg19 text, genomic_pos text, symbol text, alias text, PRIMARY KEY (id, entrez_id))''')
     db_conn.commit()
 
-def _update_db(db_conn, mgs):
+def _update_db(self, mgs):
     # a wrapper of sqlite_utils.update_db for genes
-    fields = ['gene_id','pLI','mis_z','genomic_pos_hg19','genomic_pos','symbol','alias']
+    fields = ['entrez_id','pLI','mis_z','genomic_pos_hg19','genomic_pos','symbol','alias']
     # transform mgs to a dict
     data = {}
     for i in mgs:
         genes = []
+        # some genes miss ensembl ids, fill them manually for the time being
+        if i['_id'] == '7012':
+            i['ensembl'] = {'gene':'ENSG00000270141'}
+        elif i['_id'] == '6315':
+            i['ensembl'] = {'gene':'ENSG00000230223'}
+        elif i['_id'] == '2657':
+            i['ensembl'] = {'gene':'ENSG00000130283'}
+        elif i['_id'] == '84876':
+            i['ensembl'] = {'gene':'ENSG00000276045'}
+        elif i['_id'] == '9103':
+            i['ensembl'] = {'gene':'ENSG00000244682'}
+        elif 'genomic_pos_hg19' not in i:
+            self._bad_genes.append(i['_id'])
+            logging.warning('Warning: %s is not registered in ensembl' % i['_id'])
+            continue
+        '''
+        debug
+        '''
+        if 'ensembl' not in i or 'genomic_pos' not in i:
+            print json.dumps(i,indent=4)
         if isinstance(i['ensembl'], list):
-            # sometimes ensembl returns a list, each element corresponds to a gene_id
+            # sometimes ensembl returns a list, each element corresponds to an id
             genes = [j['gene'] for j in i['ensembl']]
         else:
             genes = [i['ensembl']['gene']]
@@ -30,13 +50,17 @@ def _update_db(db_conn, mgs):
                 i['exac']['all']['p_li'] if 'exac' in i and 'all' in i['exac'] else -1, #pLI
                 i['exac']['all']['mis_z'] if 'exac' in i and 'all' in i['exac'] else -1, #mis_z
                 json.dumps(i['genomic_pos_hg19'],indent=4), #genomic_pos_hg19
-                json.dumps(i['genomic_pos'],indent=4), #genomic_pos
+                json.dumps(i.get('genomic_pos',{
+                    "start": None,
+                    "chr": None,
+                    "end": None,
+                    "strand": None}),indent=4), #genomic_pos
                 i['symbol'],
                 json.dumps(i.get('alias',[]),indent=4), #alias
             ]
     # update
     update_db(
-            db_conn,
+            self.db_conn,
             'genes',
             fields,
             data
@@ -51,11 +75,37 @@ def _fetch_one(self,field):
         print 'query mygene'
         mg = my_gene(self.id)
         # update db
-        _update_db(self.db_conn, [mg])
+        _update_db(self, [mg])
         # refetch
         db_c.execute('SELECT * FROM genes WHERE id=?',(self.id,))
         db_gene = dict_factory(db_c,db_c.fetchone())
     return db_gene[field]
+
+def _fetch_many(self,field):
+    db_c = self.db_conn.cursor()
+    result = batch_query(db_c,'genes',self.ids)
+    data = {}
+    new_genes = []
+    final = {}
+    for i in result:
+        temp = dict_factory(db_c, i)
+        data[temp['id']] = temp[field]
+    for g in self.ids:
+        if g in data and data[g] != None:
+            final[g] = data[g]
+        else:
+            new_genes.append(g)
+    if new_genes:
+        print 'querying mygenes'
+        new_result = my_genes(new_genes)
+        # update database
+        _update_db(self,new_result)
+        # query again
+        new_result = batch_query(db_c,'genes',new_genes)
+        for i in new_result:
+            temp = dict_factory(db_c, i)
+            final[temp['id']] = temp[field]
+    return final
 
 class Gene(object):
     def __init__(self, db_conn, id=None):
@@ -67,11 +117,11 @@ class Gene(object):
         self.db_conn = db_conn
     
     @property
-    def gene_id(self):
+    def entrez_id(self):
         # check local database first. if na, use CommonFuncs to annotate, then store in db
-        if getattr(self, '_gene_id', None) is None:
-            self._gene_id = _fetch_one(self,'gene_id')
-        return self._gene_id
+        if getattr(self, '_entrez_id', None) is None:
+            self._entrez_id = _fetch_one(self,'entrez_id')
+        return self._entrez_id
 
     @property
     def pLI(self):
@@ -115,33 +165,6 @@ class Gene(object):
             self._alias = json.loads(_fetch_one(self,'alias'))
         return self._alias
 
-def _fetch_many(self,field):
-    db_c = self.db_conn.cursor()
-    result = batch_query(db_c,'genes',self.ids)
-    data = {}
-    new_genes = []
-    final = {}
-    for i in result:
-        temp = dict_factory(db_c, i)
-        data[temp['id']] = temp[field]
-    for g in self.ids:
-        if g in data and data[g] != None:
-            final[g] = data[g]
-        else:
-            new_genes.append(g)
-    if new_genes:
-        print new_genes
-        print 'querying mygenes'
-        new_result = my_genes(new_genes)
-        # update database
-        _update_db(self.db_conn,new_result)
-        # query again
-        new_result = batch_query(db_c,'genes',new_genes)
-        for i in new_result:
-            temp = dict_factory(db_c, i)
-            final[temp['id']] = temp[field]
-    return final
-
 class Genes(object):
     def __init__(self, db_conn, ids=[]):
         # id ok?
@@ -150,44 +173,44 @@ class Genes(object):
         _initiate_db(db_conn)
         self.db_conn = db_conn
         self.ids = ids
+        self._bad_genes = [] # this is for bad ids that have no ordinary entries in mygenes. Avoid repetitive queries.
     
-    def gene_ids_to_ids(self,gene_ids=[]):
-        # convert from gene_ids to ids
+    def entrez_ids_to_ensembl_ids(self,entrez_ids=[]):
+        # convert from entrez_ids to ensembl ids
         db_c = self.db_conn.cursor()
-        db_result = batch_query(db_c,'genes',gene_ids,'gene_id')
+        db_result = batch_query(db_c,'genes',entrez_ids,'entrez_id')
         new_genes = []
         data = {}
         final = {}
         for i in db_result:
             temp = dict_factory(db_c,i)
-            data[temp['gene_id']] = data.get(temp['gene_id'],[])
-            data[temp['gene_id']].append(temp['id'])
-        for g in gene_ids:
+            data[temp['entrez_id']] = data.get(temp['entrez_id'],[])
+            data[temp['entrez_id']].append(temp['id'])
+        for g in entrez_ids:
             if g in data and data[g] != None:
                 final[g] = data[g]
-            else:
+            elif g not in self._bad_genes:
                 new_genes.append(g)
         if new_genes:
-            print new_genes
             print 'querying mygenes'
             new_result = my_genes(new_genes)
             # update database
-            _update_db(self.db_conn,new_result)
+            _update_db(self,new_result)
             # query again
-            new_result = batch_query(db_c,'genes',new_genes,'gene_id')
+            new_result = batch_query(db_c,'genes',new_genes,'entrez_id')
             for i in new_result:
                 temp = dict_factory(db_c, i)
-                final[temp['gene_id']] = final.get(temp['gene_id'],[])
-                final[temp['gene_id']].append(temp['id'])
+                final[temp['entrez_id']] = final.get(temp['entrez_id'],[])
+                final[temp['entrez_id']].append(temp['id'])
         return final
 
     
     @property
-    def gene_id(self):
+    def entrez_id(self):
         # check local database first. if na, use CommonFuncs to annotate, then store in db
-        if getattr(self, '_gene_id', None) is None:
-            self._gene_id = _fetch_many(self,'gene_id')
-        return self._gene_id
+        if getattr(self, '_entrez_id', None) is None:
+            self._entrez_id = _fetch_many(self,'entrez_id')
+        return self._entrez_id
     
     @property
     def pLI(self):
--- HPO.py
+++ HPO.py
@@ -5,14 +5,33 @@ import requests
 import json
 import os
 import time
+import Genes
 from CommonFuncs import *
+from collections import defaultdict
 from sqlite_utils import *
+import gzip
 
 def _initiate_db(db_conn):
     db_c = db_conn.cursor()
     db_c.execute('''CREATE TABLE IF NOT EXISTS hpo
-        (id text PRIMARY KEY UNIQUE, name text, parents text, ancestors text)''')
+        (id text PRIMARY KEY UNIQUE, name text, parents text, ancestors text, genes)''')
     db_conn.commit()
+
+def _flatten_array_of_arrays(arrs):
+    # flatten array of arrays
+    result = []
+    [result.extend(i) for i in arrs]
+    return result
+
+def _fetch_one(self,field):
+    db_c = self.db_conn.cursor()
+    db_c.execute('SELECT * FROM hpo WHERE id=?',(self.id,))
+    db_hpo = dict_factory(db_c,db_c.fetchone())
+    if db_hpo == None or db_hpo[field] == None:
+        # first check if hpo db has been constructed
+        self._check_db()
+        raise ValueError('no %s can be retrieved' % field)
+    return db_hpo[field]
 
 class Hpo:
     def __init__(self,id,db_conn):
@@ -29,7 +48,7 @@ class Hpo:
             anc.extend(is_a)
             for i in is_a:
                 return self._find_ancestors(i,anc,data)
-            
+
     def _check_db(self):
         # hpo has been constructed? if not, raise error. if yes, do nothing
         db_c = self.db_conn.cursor()
@@ -37,13 +56,27 @@ class Hpo:
         db_hpo = dict_factory(db_c,db_c.fetchone())
         if db_hpo == None:
             raise ValueError('No data seems to be in the hpo database. Did you forget to load it using HPO.construct_db("hp.obo")?')
-    def construct_db(self,obofile):
+    def construct_db(self,obofile,hpo_gene_file):
         # construct hpo database using the obo file
         data = obo_parser(obofile)
         # get ancestors
         for k,v in data.iteritems():
             data[k]['ancestors'] = self._find_ancestors(k,[],data)
         
+        # get hpo_gene
+        hpo_gene = defaultdict(list)
+        if hpo_gene_file.endswith('gz'):
+            f = gzip.open(hpo_gene_file,'rb')
+        else:
+            f = open(hpo_gene_file,'r')
+        for l in f:
+            if l[0] == '#': continue
+            l = l.rstrip().split('\t')
+            hpo_gene[l[0]].append(l[2])
+        #convert hpo_gene's gene_ids to ensembl ids
+        G = Genes.Genes(self.db_conn)
+        for k,v in hpo_gene.iteritems():
+            hpo_gene[k] = _flatten_array_of_arrays(G.entrez_ids_to_ensembl_ids(v).values())
         # convert to array of tuples
         values = []
         for k,v in data.iteritems():
@@ -51,54 +84,34 @@ class Hpo:
                 k,
                 v['name'],
                 json.dumps(v['is_a'],indent=4),
-                json.dumps(v['ancestors'],indent=4)
+                json.dumps(v['ancestors'],indent=4),
+                json.dumps(hpo_gene[k],indent=4)
             ))
         # write to database
         db_c = self.db_conn.cursor()
-        sql = 'INSERT INTO hpo VALUES (?,?,?,?)'
+        sql = 'INSERT INTO hpo VALUES (?,?,?,?,?)'
         db_c.executemany(sql,values)
         self.db_conn.commit()
     @property
     def name(self):
         if getattr(self,'_name',None) is None:
-            db_c = self.db_conn.cursor()
-            db_c.execute('SELECT * FROM hpo WHERE id=?',(self.id,))
-            db_hpo = dict_factory(db_c,db_c.fetchone())
-            if db_hpo == None or db_hpo['name'] == None:
-                # first check if hpo db has been constructed
-                _check_db()
-                raise 'no name can be retrieved'
-            else:
-                name = db_hpo['name']
-            self._name = name
+            self._name = _fetch_one(self,'name')
         return self._name
     
     @property
     def parents(self):
         if getattr(self,'_parents',None) is None:
-            db_c = self.db_conn.cursor()
-            db_c.execute('SELECT * FROM hpo WHERE id=?',(self.id,))
-            db_hpo = dict_factory(db_c,db_c.fetchone())
-            if db_hpo == None or db_hpo['parents'] == None:
-                # first check if hpo db has been constructed
-                _check_db()
-                raise ValueError('no parents can be retrieved')
-            else:
-                parents = json.loads(db_hpo['parents'])
-            self._parents = parents
+            self._parents = json.loads(_fetch_one(self,'parents'))
         return self._parents
 
     @property
     def ancestors(self):
         if getattr(self,'_ancestors',None) is None:
-            db_c = self.db_conn.cursor()
-            db_c.execute('SELECT * FROM hpo WHERE id=?',(self.id,))
-            db_hpo = dict_factory(db_c,db_c.fetchone())
-            if db_hpo == None or db_hpo['ancestors'] == None:
-                # first check if hpo db has been constructed
-                _check_db()
-                raise ValueError('no ancestors can be retrieved')
-            else:
-                ancestors = json.loads(db_hpo['ancestors'])
-            self._ancestors = ancestors
+            self._ancestors = json.loads(_fetch_one(self,'ancestors'))
         return self._ancestors
+
+    @property
+    def genes(self):
+        if getattr(self,'_genes',None) is None:
+            self._genes = json.loads(_fetch_one(self,'genes'))
+        return self._genes
