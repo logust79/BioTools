747167590b1e1dae3ee3ef60b202ae6bb22824e4
logust79 <logust@yahoo.com>
2017-02-28 14:58:14 +0000

    removed redundant code in tests/helper.py

--- tests/helper.py
+++ tests/helper.py
@@ -19,9 +19,3 @@ def compare_excel(file1, file2, sheet, key, fields):
     df2 = f2.parse(sheet)
     result = compare_dfs(df1,df2,key,fields)
     return result
-    # NaN != NaN, so replace NaN with None
-    diff_cols = df1.irow(1)[fields].replace([NaN],None)!=df2.irow(1)[fields].replace([NaN],None)
-    cols = [colname for colname, diff_cols in zip(fields, diff_cols.values) if diff_cols]
-    print(cols)
-    print(set(df1[key]))
-    print(df1[df1[key].isin([1,2])])
