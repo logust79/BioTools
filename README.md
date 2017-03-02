7b40dd9117d9277b6558d13bcd5a2777e54a5462
logust79 <logust@yahoo.com>
2017-03-02 11:18:58 +0000

    update README, and corrected a bug in test

--- README.md
+++ README.md
@@ -20,3 +20,82 @@ db_conn=sqlite3.connect('irdc.db')
 V = Variants.Variants(db_conn,['20-61523355-T-C','X-153694021-C-T'])
 print json.dumps(V.exac, indent=4)
 ```
+# Compare dataframes
+In the field of genetic diagosis, end results are refreshed when new evidence is introduced. Manual inspection necessitates highlight of changes to avoid wasting time on unchanged data.
+```
+import Compare
+import pandas as pd
+
+fields_to_check = {
+    'field1_to_check':None,
+    'field2_to_check':None,
+}
+Compare.compare_dfs('original_df', 'new_df', 'index', fields_to_check)
+```
+```
+{
+    '+':['index1','index3'],
+    '-':['index2','index6'],
+    '<>':{
+        'index4':{
+            'change':{
+                'field1_to_check':{
+                    'from':'foo',
+                    'to':'bar'
+                }
+            }
+        }
+    }
+}
+```
+You can also pass your customised compare functions as values of fields for alternative comparisons
+```
+import Compare
+import pandas as pd
+
+def filter_cb(a,b):
+    dc = {
+        'PASS':3,
+        'VQSR':2,
+        'FAIL':1,
+    }
+    if dc[b] > dc[a]: return True
+    else: return False
+
+fields_to_check = {
+    'FILTER':filter_cb,
+    'field2_to_check':None,
+}
+Compare.compare_dfs('original_df', 'new_df', 'index', fields_to_check)
+```
+Or you can use closures to create some even more customised comparison methods (useful when you want to variate some cutoffs.
+
+```
+import Compare
+import pandas as pd
+
+def exac_cb_factory(thrd):
+    def exac_cb_inner(a,b):
+        # convert np.nan to None
+        # since np.nan != np.nan
+        a = None if pd.isnull(a) else a
+        b = None if pd.isnull(b) else b
+        # equal?
+        if a == b: return False
+        # one > thrd, one < thrd?
+        if sorted([a,b,thrd])[1] == thrd: return True
+        # both > thrd?
+        if min(a,b,thrd) == thrd: return False
+        else: return True
+    
+    return exac_cb_inner
+
+exac_cutoff = 0.01
+exac_compare = exac_cb_factory(exac_cutoff)
+fields_to_check = {
+    'EXAC_AF':exac_compare,
+    'field2_to_check':None,
+}
+Compare.compare_dfs('original_df', 'new_df', 'index', fields_to_check)
+```
+When `None` is given as field value, it uses good old `==` for comparison
--- tests/test_Compare.py
+++ tests/test_Compare.py
@@ -15,14 +15,14 @@ class CompareTestCase(unittest.TestCase):
         self.assertEqual(case['<>'], dict())
 
     def test_changes(self):
-        answer = json.dumps({1: {'change': {'field5': {'to': 0.20000000000000001, 'from': 0.29999999999999999}}}, 2: {'change': {'field5': {'to': 0.01, 'from': nan}}}, 4: {'change': {'field1': {'to': u'd', 'from': u'a'}, 'field5': {'to': 0.040000000000000001, 'from': 0.0040000000000000001}}}, 5: {'change': {'field1': {'to': u'e', 'from': u'a'}, 'field5': {'to': 0.00040000000000000002, 'from': 0.00029999999999999997}}}})
+        answer = json.dumps({"1": {"change": {"field5": {"to": 0.2, "from": 0.3}}}, "2": {"change": {"field5": {"to": 0.01, "from": NaN}}}, "4": {"change": {"field1": {"to": "d", "from": "a"}, "field5": {"to": 0.04, "from": 0.004}}}, "5": {"change": {"field1": {"to": "e", "from": "a"}, "field5": {"to": 0.0004, "from": 0.0003}}}})
         case = compare_excel(os.path.join(self.datapath,'compare_old.xlsx'),os.path.join(self.datapath,'compare_new2.xlsx'),'Sheet1','key',{'field1':None,'field5':None})
         self.assertEqual(case['+'], set([]))
         self.assertEqual(case['-'], set([]))
         self.assertEqual(json.dumps(case['<>']), answer)
 
     def test_change_methods(self):
-        answer = json.dumps({1: {'change': {'field4': {'to': u'PASS', 'from': u'VQSR'}}}, 2: {'change': {'field5': {'to': 0.01, 'from': nan}}}, 4: {'change': {'field1': {'to': u'd', 'from': u'a'}, 'field5': {'to': 0.040000000000000001, 'from': 0.0040000000000000001}}}, 5: {'change': {'field1': {'to': u'e', 'from': u'a'}, 'field5': {'to': 0.00040000000000000002, 'from': 0.00029999999999999997}}}})
+        answer = json.dumps({"1": {"change": {"field4": {"to": "PASS", "from": "VQSR"}}}, "2": {"change": {"field5": {"to": 0.01, "from": NaN}}}, "4": {"change": {"field1": {"to": "d", "from": "a"}, "field5": {"to": 0.04, "from": 0.004}}}, "5": {"change": {"field1": {"to": "e", "from": "a"}, "field5": {"to": 0.0004, "from": 0.0003}}}})
         field5_closure = field5_cb_factory(0.01)
         case =  compare_excel(os.path.join(self.datapath,'compare_old.xlsx'),os.path.join(self.datapath,'compare_new2.xlsx'),'Sheet1','key',{'field1':None, 'field4':field4_cb, 'field5':field5_closure})
         self.assertEqual(case['+'], set([]))
@@ -36,7 +36,7 @@ class CompareTestCase(unittest.TestCase):
         self.assertEqual(case['<>'], dict())
 
     def test_add(self):
-        answer = json.dumps({3: {'change': {'field5': {'to': 0.10000000000000001, 'from': nan}}}})
+        answer = json.dumps({"3": {"change": {"field5": {"to": 0.1, "from": NaN}}})
         case = compare_excel(os.path.join(self.datapath,'compare_old.xlsx'),os.path.join(self.datapath,'compare_new4.xlsx'),'Sheet1','key',{'field1':None,'field5':None})
         self.assertEqual(case['+'], set([6]))
         self.assertEqual(case['-'], set([]))
