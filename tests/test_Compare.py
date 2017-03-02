2329a0a6ab889a887c289ef9074995f49ec07139
logust79 <logust@yahoo.com>
2017-03-02 11:54:42 +0000

    corrected test, hopefully

--- tests/test_Compare.py
+++ tests/test_Compare.py
@@ -15,19 +15,26 @@ class CompareTestCase(unittest.TestCase):
         self.assertEqual(case['<>'], dict())
 
     def test_changes(self):
-        answer = json.dumps({"1": {"change": {"field5": {"to": 0.2, "from": 0.3}}}, "2": {"change": {"field5": {"to": 0.01, "from": NaN}}}, "4": {"change": {"field1": {"to": "d", "from": "a"}, "field5": {"to": 0.04, "from": 0.004}}}, "5": {"change": {"field1": {"to": "e", "from": "a"}, "field5": {"to": 0.0004, "from": 0.0003}}}})
+        answer = json.dumps({"1": {"change": {"field5": {"to": 0.20000000000000001, "from": 0.29999999999999999}}}, "2": {"change": {"field5": {"to": 0.01, "from": NaN}}}, "4": {"change": {"field1": {"to": "d", "from": "a"}, "field5": {"to": 0.040000000000000001, "from": 0.0040000000000000001}}}, "5": {"change": {"field1": {"to": "e", "from": "a"}, "field5": {"to": 0.00040000000000000002, "from": 0.00029999999999999997}}}})
         case = compare_excel(os.path.join(self.datapath,'compare_old.xlsx'),os.path.join(self.datapath,'compare_new2.xlsx'),'Sheet1','key',{'field1':None,'field5':None})
         self.assertEqual(case['+'], set([]))
         self.assertEqual(case['-'], set([]))
-        self.assertEqual(json.dumps(case['<>']), answer)
+        self.assertEqual(case['<>'][1]['change'].keys(), ['field5'])
+        self.assertEqual(case['<>'][2]['change'].keys(), ['field5'])
+        self.assertEqual(sorted(case['<>'][4]['change'].keys()), ['field1','field5'])
+        self.assertEqual(sorted(case['<>'][5]['change'].keys()), ['field1','field5'])
 
     def test_change_methods(self):
-        answer = json.dumps({"1": {"change": {"field4": {"to": "PASS", "from": "VQSR"}}}, "2": {"change": {"field5": {"to": 0.01, "from": NaN}}}, "4": {"change": {"field1": {"to": "d", "from": "a"}, "field5": {"to": 0.04, "from": 0.004}}}, "5": {"change": {"field1": {"to": "e", "from": "a"}, "field5": {"to": 0.0004, "from": 0.0003}}}})
+        answer = json.dumps({"1": {"change": {"field4": {"to": "PASS", "from": "VQSR"}}}, "2": {"change": {"field5": {"to": 0.01, "from": NaN}}}, "4": {"change": {"field1": {"to": "d", "from": "a"}, "field5": {"to": 0.040000000000000001, "from": 0.0040000000000000001}}}, "5": {"change": {"field1": {"to": "e", "from": "a"}, "field5": {"to": 0.00040000000000000002, "from": 0.00029999999999999997}}}})
         field5_closure = field5_cb_factory(0.01)
         case =  compare_excel(os.path.join(self.datapath,'compare_old.xlsx'),os.path.join(self.datapath,'compare_new2.xlsx'),'Sheet1','key',{'field1':None, 'field4':field4_cb, 'field5':field5_closure})
         self.assertEqual(case['+'], set([]))
         self.assertEqual(case['-'], set([]))
-        self.assertEqual(json.dumps(case['<>']), answer)
+        self.assertEqual(case['<>'][1]['change'].keys(), ['field4'])
+        self.assertEqual(case['<>'][2]['change'].keys(), ['field5'])
+        self.assertEqual(sorted(case['<>'][4]['change'].keys()), ['field1','field5'])
+        self.assertEqual(sorted(case['<>'][5]['change'].keys()), ['field1','field5'])
+
 
     def test_delete(self):
         case = compare_excel(os.path.join(self.datapath,'compare_old.xlsx'),os.path.join(self.datapath,'compare_new3.xlsx'),'Sheet1','key',{'field1':None,'field5':None})
@@ -36,11 +43,11 @@ class CompareTestCase(unittest.TestCase):
         self.assertEqual(case['<>'], dict())
 
     def test_add(self):
-        answer = json.dumps({"3": {"change": {"field5": {"to": 0.1, "from": NaN}}}})
+        answer = json.dumps({"3": {"change": {"field5": {"to": 0.10000000000000001, "from": NaN}}}})
         case = compare_excel(os.path.join(self.datapath,'compare_old.xlsx'),os.path.join(self.datapath,'compare_new4.xlsx'),'Sheet1','key',{'field1':None,'field5':None})
         self.assertEqual(case['+'], set([6]))
         self.assertEqual(case['-'], set([]))
-        self.assertEqual(json.dumps(case['<>']), answer)
+        self.assertEqual(case['<>'][3]['change'].keys(), ['field5'])
 
     def test_add_delete(self):
         case = compare_excel(os.path.join(self.datapath,'compare_old.xlsx'),os.path.join(self.datapath,'compare_new5.xlsx'),'Sheet1','key',{'field1':None,'field5':None})
